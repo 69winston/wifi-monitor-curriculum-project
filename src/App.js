2aa8aa3bdbd423511e632411342554e50e7f3cc5
69winston <114656625+69winston@users.noreply.github.com>
2024-03-07 02:48:25 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
   const [searchHistory, setSearchHistory] = useState([]);
 
   useEffect(() => {
-    // Retrieve search history from localStorage when component mounts
-    const savedSearchHistory = localStorage.getItem('searchHistory');
-    if (savedSearchHistory) {
-      setSearchHistory(JSON.parse(savedSearchHistory));
-    }
+    // Fetch search history from server when component mounts
+    fetch('/PORT/get-users')
+      .then(response => response.json())
+      .then(data => {
+        setSearchHistory(data.results);
+      })
+      .catch(error => {
+        console.error('Error fetching search history:', error);
+      });
   }, []); // Empty dependency array to run this effect only once when the component mounts
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newSearchHistory = [...searchHistory, searchTerm];
+    const newSearchHistory = [...searchHistory, `${selectedOption}: ${searchTerm}`];
     setSearchHistory(newSearchHistory);
-    localStorage.setItem('searchHistory', JSON.stringify(newSearchHistory)); // Update localStorage
+
+    // POST new search term to the server
+    fetch('/add-user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        location: selectedOption,
+        rating: searchTerm
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle response if needed
+      console.log('Response from server:', data);
+    })
+    .catch(error => {
+      console.error('Error posting search term:', error);
+    });
+
     setSearchTerm('');
+    setSelectedOption('');
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={handleSubmit}>
-          <select id="followersDropdown">
+          <select id="followersDropdown" onChange={handleDropdownChange} value={selectedOption}>
+            <option value="">Select an option</option>
             <option value="MLK">MLK</option>
             <option value="Moffit">Moffit</option>
             <option value="Mainstacks">Mainstacks</option>
